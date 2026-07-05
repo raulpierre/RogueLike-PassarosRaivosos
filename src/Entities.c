3c1e297791824a60b0127c0ed271f04385daedff
asteri-0 <linlopesg07@gmail.com>
2026-07-04 22:37:37 -0300

    começo da implementação do id global

--- src/Entities.c
+++ src/Entities.c
@@ -1,6 +1,17 @@
 #include "Entities.h"
-#include "raylib.h"
+#include "Physics.h"
+#include "raylib/raylib.h"
+
+static uint32_t global_id_count = 1;
+
+Entities* CreateBlock(float x, float y, int typeMaterial) {
+
+}
 
 void DrawEntities(Vector2 position, int width, int height, Color color){
     DrawRectangle(position.x, position.y, width, height, color);
+}
+
+void updateEntities(Entities *ent, float delta) {
+
 }
