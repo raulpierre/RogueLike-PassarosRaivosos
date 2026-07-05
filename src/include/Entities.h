0fa258fa2680dacb5d007be018c6a0d14f77e042
asteri-0 <linlopesg07@gmail.com>
2026-07-04 22:38:34 -0300

    declaração do createblock

--- src/include/Entities.h
+++ src/include/Entities.h
@@ -1,11 +1,17 @@
 #pragma once
-#include <raylib.h>
+#include "raylib/raylib.h"
+#include <stdint.h>
 
-typedef struct Entities{
+typedef struct Entities {
     Vector2 position;
     int mass;
     int width;
     int height;
+    uint32_t id;
 }Entities;
 
 void DrawEntities(Vector2 position, int width, int height, Color color);
+
+void updateEntities(Entities *ent, float delta);
+
+Entities* CreateBlock(float x, float y, int typeMaterial);
