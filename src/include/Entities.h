fa71072c74fe28dd14aefe8b4eaf1b0bc9c723f9
asteri-0 <linlopesg07@gmail.com>
2026-07-08 16:52:31 -0300

    fisica nascendo

--- src/include/Entities.h
+++ src/include/Entities.h
@@ -1,6 +1,9 @@
 #pragma once
+
 #include "raylib/raylib.h"
+#include "Physics.h"
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct Entities {
     Vector2 position;
@@ -8,10 +11,15 @@ typedef struct Entities {
     int width;
     int height;
     uint32_t id;
+    bool active;
+    bool isMaterial;
+    TypeEntities entityTypo;
 }Entities;
 
 void DrawEntities(Vector2 position, int width, int height, Color color);
 
 void updateEntities(Entities *ent, float delta);
+
+PhysicalEntity* searchEmptySlot(void);
 
 Entities* CreateBlock(float x, float y, int typeMaterial);
